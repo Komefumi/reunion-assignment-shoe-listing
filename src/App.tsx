a5eca0e1f3f93de57b17cca9a9234765f3d00245
Komefumi <komefumi@protonmail.ch>
2022-01-26 18:04:00 +0530

    complete upto header of the panel (exclusively in terms of ui and can be reiterated later)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import DimBackground from "@ui/DimBackground";
 import Panel from "@ui/DisplayPanel";
+import SearchIconSVG from "@assets/search-icon.svg";
 import classes from "./App.module.scss";
 
 export default function App() {
+  console.log({ SearchIconSVG });
   return (
     <div className={classes.app}>
       <DimBackground className={classes.background}>
         <Panel
           className={classes.panel}
-          headerChildren={<div>Header Content</div>}
+          headerChildren={
+            <div className={classes.panel_header_content}>
+              <h4 className={classes.title}>Shoe</h4>
+
+              <div className={classes.search_bar}>
+                <SearchIconSVG className={classes.search_icon} />
+                <input className={classes.search_input} type="text" />
+              </div>
+            </div>
+          }
         >
           <div>Body Content</div>
         </Panel>
