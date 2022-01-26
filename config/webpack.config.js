2cb019d7ac05f6f5f370e8ba558a6b36ba7d249e
Komefumi <komefumi@protonmail.ch>
2022-01-26 14:19:02 +0530

    add .json to resolve extensions

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = webpackMerge(
     ],
     resolve: {
       plugins: [new TsconfigPathsPlugin({ configFile: tsconfigFilePath })],
-      extensions: ["", ".js", ".ts", ".tsx"],
+      extensions: ["", ".js", ".json", ".ts", ".tsx"],
     },
   },
   scriptsConfig,
