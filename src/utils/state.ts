36b7f8e6ba95860a321fca813776a430f6e0f3f0
Komefumi <komefumi@protonmail.ch>
2022-01-27 11:00:43 +0530

    fix issue - make sure innerValue is assigned to payload of returning object and not becoming a property of a nested object - viz. need ({ payload: innerValue }) and not ({ payload: { innerValue } })

--- src/utils/state.ts
+++ src/utils/state.ts
@@ -2,8 +2,6 @@ import { IPayloadObject } from "@my-types/state";
 
 export function createPayloadObject(innerValue: any): IPayloadObject<typeof innerValue> {
   return {
-    payload: {
-      innerValue,
-    },
+    payload: innerValue,
   };
 }
