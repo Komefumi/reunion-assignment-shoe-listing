c5b5f146a0480aa16a0db9321dee533b6d9dc760
Komefumi <komefumi@protonmail.ch>
2022-01-27 20:31:05 +0530

    Remove unnecessary global styles 'crisp-light-text' and move it back to local usage as 'crisp_light_text' within src/components/ui/ProductCard

--- src/components/ui/ProductCard/index.tsx
+++ src/components/ui/ProductCard/index.tsx
@@ -17,18 +17,14 @@ export default function ProductCard({
             classes[`visual_side_handle_${sideHandleMode.toLowerCase()}`]
           )}
         ></span>
-        <h6 className={clsx("crisp-light-text", classes.subtitle)}>
-          {product.subtitle}
-        </h6>
+        <h6 className={classes.subtitle}>{product.subtitle}</h6>
         <h5 className={classes.name}>{product.name}</h5>
         <div className={classes.main_display}>
           <img src={imageData.main} alt="A main display for the product" />
         </div>
         <footer className={classes.footer}>
           <div className={classes.price_display}>
-            <label className={clsx("crisp-light-text", classes.price_label)}>
-              Price
-            </label>
+            <label className={classes.price_label}>Price</label>
             <div className={classes.price_value}>
               <span className={classes.price_currency_symbol}>&#36;</span>
               <span className={classes.price_amount}>{product.price}</span>
